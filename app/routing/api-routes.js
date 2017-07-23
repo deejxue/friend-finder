73852cc69be88c9f3ed388845c4f0190a6bb1f94
Alex DJ <deejxue@gmail.com>
2017-07-23 13:06:15 -0700

    final commit

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -33,14 +33,14 @@ routes.post('/api/friends', function(req, res){
 
 	// Calculate friends total scores
 		for (i=0; i<friends.length; i++) {
-		  var friendsTotalScore = sum(friends[i].scores);
+		  var friendsScoreTotal = sum(friends[i].scores);
 		}
 
 	// Determine closest match
 		var differencesArray = [];
 		for (i=0; i<friends.length; i++) {
-			var totalDifference = Math.abs(myTotalScore - sum(friends[i].scores));
-			differencesArray.push(totalDifference);
+			var differenceTotal = Math.abs(myTotalScore - sum(friends[i].scores));
+			differencesArray.push(differenceTotal);
 		}
 		var min = Math.min.apply(Math, differencesArray);
 		var x = differencesArray.indexOf(min);
