9337bc4b9d2dde0a32b1e67505eceb5efb400b80
Alex DJ <deejxue@gmail.com>
2017-08-16 11:40:10 -0700

    adding CSS style, removing inline styles, added STATIC file route, adding imgs

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
+// For my static files and the CSS to work properly
+app.use(express.static(__dirname + '/app/public'));
+
 // Routes
 app.use('/', htmlRoutes);
 app.use('/survey', htmlRoutes);
