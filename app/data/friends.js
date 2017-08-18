9c7d6410b67ded9fbd16d25586d374eaf7933ce4
Alex DJ <deejxue@gmail.com>
2017-08-17 17:13:06 -0700

    fix images and links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,26 @@
 var friends = [
   { name: 'Wolverine',
-  photo: 'http://comicsalliance.com/files/2013/02/40-blacksad.jpg',
+  photo: './img/wolverine.jpg',
   scores: [ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' ] },
 
+  { name: 'Myne Cavalier',
+  photo: './img/minecraft.warrior.jpg',
+  scores: [ '2', '2', '2', '2', '2', '2', '2', '2', '2', '2' ] },
+
   { name: 'Tony the Tiger',
-  photo: 'http://i2.kym-cdn.com/photos/images/newsfeed/001/103/281/dc5.jpg',
+  photo: './img/tonytiger.jpg',
   scores: [ '3', '3', '3', '3', '3', '3', '3', '3', '3', '3' ] },
 
+  { name: 'The booby prize....Cowboy Burger Turtle',
+  photo: './img/walkthiswayburger.png',
+  scores: [ '1', '2', '2', '3', '3', '3', '4', '4', '4', '5' ] },
+
   { name: 'Cat Woman',
-  photo: 'http://img14.deviantart.net/66a7/i/2009/071/2/4/animated_catwoman_2_by_d3rx.png',
+  photo: './img/catwoman.png',
   scores: [ '4', '4', '4', '4', '4', '4', '4', '4', '4', '4' ] },
 
   { name: 'Turanga Leela',
-  photo: 'http://img.cinemablend.com/cb/c/4/a/4/e/d/c4a4ede4d4c40959923d56c5b818f88f460242dc816a7aad3c262ffe847598bf.jpg',
+  photo: './img/leelanude.jpg',
   scores: [ '5', '5', '5', '5', '5', '5', '5', '5', '5', '5' ] }
 ];
 
